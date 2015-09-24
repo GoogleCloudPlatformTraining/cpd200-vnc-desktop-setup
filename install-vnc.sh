72abfeabae093b201cf4841c270deb9dad8f696e
Sharif Salah <sharifsalah@users.noreply.github.com>
2015-09-24 10:28:20 +0100

    Update install-vnc.sh
    Moved gnome-core to separate install step with --no-install-recommends

--- install-vnc.sh
+++ install-vnc.sh
@@ -20,11 +20,11 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 dpkg --add-architecture i386
 apt-get update
+apt-get install -y gnome-core --no-install-recommends
 apt-get install -y google-chrome-stable \
                    zip \
                    openjdk-7-jdk \
                    vnc4server \
-                   gnome-core \
                    ia32-libs \
                    lib32ncurses5-dev \
                    lib32stdc++6
