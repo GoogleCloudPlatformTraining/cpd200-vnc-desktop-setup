1ebe41dec0119c51502c980b36e583190b4c16cf
Sharif Salah <sharifsalah@users.noreply.github.com>
2015-09-24 12:02:17 +0100

    Update install-vnc.sh
    Suppressed apt-get update noise

--- install-vnc.sh
+++ install-vnc.sh
@@ -19,7 +19,7 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 dpkg --add-architecture i386
-apt-get update
+apt-get -qq update
 apt-get install -y -qq gnome-core --no-install-recommends
 apt-get install -y -qq google-chrome-stable \
                        git \
